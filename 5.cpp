b78b594cec930ccbdb2f7206556dfbb651aa5083
jdpchong <90409232+jdpchong@users.noreply.github.com>
2021-10-30 14:58:26 +0800

    修改版

--- 5.cpp
+++ 5.cpp
@@ -1,10 +1,18 @@
 string expression()
-{
-	for(int i=0;i<=rand()%3;i++)
 	{
-		str.append(student.RandomNum);
-		str.append(student.RandomOp);
+		string str="";
+		char ch1='(',ch2=')';
+		int number_num=rand()%3+1;		//操作数的个数 
+		if(number_num>=2) 
+			str+=ch1;			//个数大于2才加括号 
+		for(int i=0;i<number_num;i++)
+		{
+			str.append(RandomNum());
+			if(number_num>=2&&i==1)
+			str+=ch2;
+			str.append(RandomOp());
+		}
+		str.append(RandomNum());
+		
+		return str;
 	}
-	str.append(student.RandomNum);
-	cout<<str<<"="<<"?"<<endl
-}
