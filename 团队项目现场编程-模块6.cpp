a8432df9235fd0e7a7670ba65e9b8583bfed69d6
LiangYC1021 <56441078+LiangYC1021@users.noreply.github.com>
2021-10-30 19:11:49 +0800

    Update 团队项目现场编程-模块6.cpp
    添加了包含括号的计算功能。
    请使用Getvalue函数。

--- 团队项目现场编程-模块6.cpp
+++ 团队项目现场编程-模块6.cpp
@@ -23,6 +23,7 @@ string dou_to_str(double x){
 	ss>>res;
 	return res;
 }
+
 double getres(string s){
 	double res=0;
 	string cur="";
@@ -81,14 +82,43 @@ double getres(string s){
 	}
 	return str_to_dou(v[0]);
 }
+
+double GetValue(string s)//use this to calculate string which contains '(' and ')'
+{
+	string tmp="",t="";
+	string::iterator it=s.begin(),it1;
+	for(;it!=s.end();it++){
+		if((*it)==')'){
+			it1=it-1;
+			while((*it1)!='('){
+				tmp=(*it1)+tmp;
+				it1--;
+			}
+			tmp=dou_to_str(getres(tmp));
+			while(1){
+				char ch=*it1;
+				s.erase(it1);
+				if(ch==')')break;
+			}
+			int pos=it1-s.begin();
+			s.insert(pos,tmp);
+			it=it1;
+//			cout<<tmp<<endl;
+			tmp="";
+		}
+	}
+//	cout<<s<<endl;
+	return getres(s);
+}
 //-------------Module end------------------ 
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);cout.tie(0);
-//	string s="100.123/100";
-//	cout<<getres(s); 
+//	string s;
+//	cin>>s; 
+//	cout<<fixed<<setprecision(2)<<GetValue(s);
 	return 0;
 }
 
